f4843a2877971afd388b6cf6961903ce6fdf5f29
Vanka Siddhartha <97089805+vankaSiddhartha@users.noreply.github.com>
2025-01-13 17:51:10 +0530

    Splash screen ui update

---
+++ app/src/main/java/com/app/farmease/startupScreen/StartUpScreen.kt
@@ -0,0 +1,12 @@
+package com.app.farmease.startupScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.farmease.R
+
+class StartUpScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up_screen)
+    }
+}
